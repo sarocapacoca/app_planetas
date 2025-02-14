6764ef964f25ee0ccf7e4de3f8748ab7ed287754
sarahtalentotech <sarahazevedolj@gmail.com>
2025-02-13 21:34:07 -0300

    Correções no projeto

---
+++ app_planeta/lib/modelos/planeta.dart
@@ -0,0 +1,44 @@
+
+class Planeta {
+  int? id;
+  String nome;
+  double tamanho;
+  double distancia;
+  String? apelido;
+
+//Construtor da classe Planeta
+  Planeta({
+    this.id,
+    required this.nome,
+    required this.tamanho,
+    required this.distancia,
+    this.apelido,
+  });
+
+Planeta.vazio()
+:  nome = '',
+  tamanho = 0.01,
+  distancia = 0.0,
+  apelido = '';
+
+  factory Planeta.fromMap(Map<String, dynamic> map) {
+  return Planeta(
+    id: map['id'],
+    nome: map['nome'],
+    tamanho: map['tamanho'],
+    distancia: map['distancia'],
+    apelido: map['apelido'],
+  );
+}
+
+Map<String, dynamic> toMap() {
+  return {
+    'id': id,
+    'nome': nome,
+    'tamanho': tamanho,
+    'distancia': distancia,
+    'apelido': apelido,
+  };
+}
+}
+
